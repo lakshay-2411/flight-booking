4aa65b24d46095512113771c7217b591062c539b
lakshay-2411 <lakshaynandwani06@gmail.com>
2025-05-10 15:37:55 +0530

    Fixed ticket generation

--- client/src/utils/pdfGenerator.ts
+++ client/src/utils/pdfGenerator.ts
@@ -1,5 +1,7 @@
 // utils/pdfGenerator.ts
 export const generateTicketPDF = (bookingId: string) => {
-  const ticketUrl = `${import.meta.env.VITE_BACKEND_URL}/${bookingId}`;
-  window.open(ticketUrl, '_blank');
+  const ticketUrl = `${
+    import.meta.env.VITE_BACKEND_URL
+  }/api/tickets/${bookingId}`;
+  window.open(ticketUrl, "_blank");
 };
