a963ba44144b3c5befa16eefb5a1780ed4399aa7
lakshay-2411 <lakshaynandwani06@gmail.com>
2025-05-10 14:53:50 +0530

    Fixed minor bug

--- server/src/routes/ticketRoutes.ts
+++ server/src/routes/ticketRoutes.ts
@@ -1,6 +1,6 @@
 // routes/ticketRoutes.ts
 import express from 'express';
-import { generateTicket } from '../controllers/TicketController';
+import { generateTicket } from '../controllers/ticketController';
 
 const router = express.Router();
 router.get('/tickets/:bookingId', generateTicket);
